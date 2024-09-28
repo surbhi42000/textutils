7291dacc4d1de8a6c23cc8ec7373005333fe5684
Surbhi Pareek <115787046+surbhi42000@users.noreply.github.com>
2024-09-28 12:14:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+// import About from './components/About';
+import React, { useState } from 'react';
+import Alert from './components/Alert';
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+// } from "react-router-dom";
+
+function App() {
+  const [mode, setMode] = useState('light'); // whether dark mode is enable or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success");
+      document.title = 'TextUtils - Dark Mode'
+      // setInterval(() =>{
+      //   document.title = 'TextUtils is Amazing Mode';
+      // }, 2000);
+      // setInterval(() =>{
+      //   document.title = 'Install TextUtils Now';
+      // }, 1500);
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+      document.title = 'TextUtils - Light Mode'
+    }
+
+  }
+  return (
+    <>
+      {/* <Navbar title="TextUtils" aboutText="about TextUtils" /> */}
+      {/* <navbar/> */}
+//       <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <TextForm showAlert={showAlert} heading="Enter the text to analyze belows" mode={mode} />
+//           <Routes>
+//             <Route exact path="/about" element={<About/>}/>
+//             <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze belows" mode={mode} />}/>
+//           </Routes>
+        </div>
+//       </Router>
+      {/* <About/> */}
+      {/* <Navbar title="TextUtils" />
+      <div className="container my-3">
+      <About/>
+      </div> */}
+    </>
+  );
+}
+
+export default App;
